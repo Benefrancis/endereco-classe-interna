de3381c33b6cb033f7db2d82ad7cbfc5d345b5fc
Benefrancis <benefrancis@gmail.com>
2022-12-04 17:17:19 -0300

    Atualizando README.md

--- src/main/java/br/com/benefrancis/Main.java
+++ src/main/java/br/com/benefrancis/Main.java
@@ -7,17 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Endereco e = new Endereco();
 
-        e.setCep(e.new CEP("06086-120"));
-        e.setBairro("Vila Osasco");
+        e.setCep(e.new CEP("06086-120")).setBairro("Vila Osasco");
 
         Endereco.Pais pais = e.new Pais("Brasil");
         Endereco.Estado estado = e.new Estado("São Paulo", pais);
         Endereco.Cidade cidade = e.new Cidade("Osasco", estado);
 
-        e.setCidade(cidade);
-        e.setComplemento("Ap 81-A");
-        e.setLogradouro("Rua Adolpho Bozzi");
-        e.setNumero("505");
+        e.setCidade(cidade).setComplemento("Ap 81-A");
+        e.setLogradouro("Rua Adolpho Bozzi").setNumero("505");
         System.out.println(e);
     }
 }
